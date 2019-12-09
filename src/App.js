601fa7eac8572c212e0955e48ec6803cffe077a5
mkortib <mkobria@gmail.com>
2019-12-10 00:10:36 +0200

    added checkout page, checkout item component and updated cart to use selector from reselect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
 	unsubscribeFromAuth = null;
@@ -44,23 +47,24 @@ class App extends React.Component {
 			<div>
 				<Header />
 				<Switch>
-					<Route exact path="/" component={HomePage} />
-					<Route path="/shop" component={ShopPage} />
+					<Route exact path='/' component={HomePage} />
+					<Route path='/shop' component={ShopPage} />
 					<Route
 						exact
-						path="/signin"
+						path='/signin'
 						render={() =>
-							this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+							this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp />
 						}
 					/>
+					<Route exact path='/checkout' component={CheckoutPage} />
 				</Switch>
 			</div>
 		);
 	}
 }
 
-const mapStateToProps = ({ user }) => ({
-	currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+	currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
+	<div className='checkout-item'>
+		<div className='image-container'>
+			<img src={imageUrl} alt='item' />
+		</div>
+		<div className='name'>{name}</div>
+		<div className='quantity'>{quantity}</div>
+		<div className='price'>{price}</div>
+		<div className='remove-button'>&#10005;</div>
+	</div>
+);
+
+export default CheckoutItem;
